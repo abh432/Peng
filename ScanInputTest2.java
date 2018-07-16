a1a7681b431af7dd980b2e195d47c80d5df558a8
abh432 <41271118+abh432@users.noreply.github.com>
2018-07-16 07:40:04 +0530

    Add files via upload

---
+++ ScanInputTest2.java
@@ -0,0 +1,9 @@
+package x;
+import java.util.*;
+public class ScanInputTest2{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		String k = s.nextLine();
+		System.out.println(k);
+	}
+}
